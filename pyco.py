40d025d674ddca8e04e4c1a90832e7789e8022d1
Lee Holmes <github@leeholmes.com>
2025-07-07 10:29:34 -0700

    Add files via upload

---
+++ pyco.py
@@ -0,0 +1,91 @@
+# pyco calculator customizations
+import sys
+from math import *
+from statistics import *
+
+if sys.implementation.name == 'cpython':
+    def displayhook(value):
+        if callable(value):
+            value()
+        else:
+            sys.__displayhook__(value)
+    sys.displayhook = displayhook
+elif sys.implementation.name == 'micropython':
+    __original_repl_print__ = __repl_print__
+    def displayhook(value):
+        if callable(value):
+            value()
+        else:
+            __original_repl_print__(value)
+    __repl_print__ = displayhook
+
+def find(term):
+    found = []
+    for current in globals():
+        if term in current:
+            found.append(current)
+    return found
+
+def asciitable():
+    for i in range(0, 255, 4):
+        if (i > 0) and (i % 72) == 0:
+            input("Press ENTER to continue...")
+        if i % 72 == 0:
+            print("Dec Hx C | Dec Hx C | Dec Hx C | Dec Hx C")
+        print("{:3d} {:02X} {} | {:3d} {:02X} {} | {:3d} {:02X} {} | {:3d} {:02X} {}".format(
+            i, i, get_printable_char(i),
+            (i + 1), (i + 1), get_printable_char(i + 1),
+            (i + 2), (i + 2), get_printable_char(i + 2),
+            (i + 3), (i + 3), get_printable_char(i + 3)))
+    return
+def at():
+    return asciitable()
+
+def get_printable_char(char):
+    if char < 32:
+        return '.'
+    elif char > 126 and char < 160:
+        return '.'
+    else:
+        return chr(char)
+
+def inputlist():
+    global _list
+    _list = []
+    inputCounter = 0
+    print()
+    while True:
+        lastInput = input(str(inputCounter) + ": ")
+        if(lastInput == ""):
+            break
+        _list.append(float(lastInput))
+        inputCounter += 1
+    return _list
+def il():
+    return inputlist()
+
+
+def average(list):
+    return mean(list)
+def avg(list):
+    return average(list)
+
+def convert_celsius_fahrenheit(celsius):
+    return (celsius * 9 / 5) + 32
+def c_c_f(celsius):
+    return convert_celsius_fahrenheit(celsius)
+def c_f(celsius):
+    return c_c_f(celsius)
+
+def convert_fahrenheit_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
+def c_f_c(fahrenheit):
+    return convert_fahrenheit_celsius(fahrenheit)
+def f_c(fahrenheit):
+    return c_f_c(fahrenheit)
+
+
+kb = 1024
+mb = kb * kb
+gb = kb * mb
+tb = kb * gb
